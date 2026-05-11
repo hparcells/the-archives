9b0736510225027022845d604d6ca462c5a3f59a
Hunter Parcells <hunterparcells@gmail.com>
2026-05-11 12:10:47 -0400

    fix: Make Docker use Infisical.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-infisical run --env=production --token=$INFISICAL_TOKEN -- docker compose pull
-infisical run --env=production --token=$INFISICAL_TOKEN -- docker compose up -d
+infisical run --env=prod --token=$INFISICAL_TOKEN -- docker compose pull
+infisical run --env=prod --token=$INFISICAL_TOKEN -- docker compose up -d
