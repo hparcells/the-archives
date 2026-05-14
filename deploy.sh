3e013bc5b71676f97e99668bf3077b2e55886d55
Hunter Parcells <hunterparcells@gmail.com>
2026-05-14 01:12:24 -0400

    chore: Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-infisical run --env=prod --token=$INFISICAL_TOKEN -- docker compose pull
-infisical run --env=prod --token=$INFISICAL_TOKEN -- docker compose up -d
+infisical run --env=prod --token=$INFISICAL_TOKEN --projectId=8204894b-8fa8-464f-a4d2-dc6f9621d7c8 --domain=https://secrets.hunterparcells.com -- docker compose pull
+infisical run --env=prod --token=$INFISICAL_TOKEN --projectId=8204894b-8fa8-464f-a4d2-dc6f9621d7c8 --domain=https://secrets.hunterparcells.com -- docker compose up -d
