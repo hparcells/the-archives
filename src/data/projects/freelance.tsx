3254b4ffcb1a322df30bb926be399a58c7aa9c08
Hunter Parcells <hunterparcells@gmail.com>
2026-05-21 15:13:50 -0400

    feat: Add freelance projects.

--- src/data/projects/freelance.tsx
+++ src/data/projects/freelance.tsx
@@ -49,6 +49,157 @@ export const FREELANCE_PROJECTS: Project[] = [
     links: {
       github: 'https://github.com/tck-gg/tck'
     },
+    featured: true
+  },
+  {
+    type: 'freelance',
+    date: '2020-11-24',
+    startDate: '2020-11-17',
+    name: 'Butterfly SMP Season 1 Mod',
+    tagline: 'Content addition mod for the Butterfly SMP server.',
+    images: [],
+    status: 'archived',
+    description: (
+      <p>
+        Added various recipes and items with custom textures and metadata. Developed for the
+        Butterfly SMP on Minecraft 1.16.4.
+      </p>
+    ),
+    technologies: ['Java', 'ForgeGradle'],
+    links: {},
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-01-11',
+    startDate: '2023-11-10',
+    name: 'Fireball Wand',
+    tagline: 'Fireball launching weapon for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>
+          Custom weapon addition utilizing custom model data for Minecraft 1.20.1. Implemented
+          enhanced player feedback using sound effects and timers.
+        </p>
+        <p>Developed for the Butterfly SMP.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {
+      github: 'https://github.com/butterfly-pit/fireballwand'
+    },
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-01-14',
+    startDate: '2024-01-10',
+    name: 'Rogue Sword',
+    tagline: 'Weapon addition for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: <p>Custom weapon addition utilizing custom model data for Minecraft 1.20.1.</p>,
+    technologies: ['Java', 'Spigot API'],
+    links: {},
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-01-14',
+    startDate: '2023-11-17',
+    name: 'Friendly Zombies',
+    tagline: 'Commandable Minecraft drowneds on-command for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>
+          Added custom on-hit effects to in-game tridents to summon drowneds to fight for the player
+          summoning.
+        </p>
+        <p>Developed for the Butterfly SMP on Minecraft 1.20.1.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {},
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-02-10',
+    startDate: '2024-01-15',
+    name: 'Persistent Chests',
+    tagline: 'Minecraft chests with persistent inventories across worlds.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>Added chests which save their contents to a YAML file using NBT-API.</p>
+        <p>Developed for the Butterfly SMP on Minecraft 1.20.1.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {},
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-02-10',
+    startDate: '2024-01-06',
+    name: 'Tomes',
+    tagline: 'Magical books and spells. Built for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>Added magical books, each with unique abilities for the Butterfly SMP.</p>
+        <p>Built for Minecraft 1.20.1.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {},
+    featured: false
+  },
+
+  {
+    type: 'freelance',
+    date: '2024-02-15',
+    startDate: '2024-01-06',
+    name: 'Adaptive Totems',
+    tagline: 'Customizable totems with buffs. Built for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>
+          Modified Minecraft totems to include an inventory which can be customized with buffs.
+          Buffs are affected by diminishing returns.
+        </p>
+        <p>Built for Minecraft 1.20.1.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {},
+    featured: false
+  },
+  {
+    type: 'freelance',
+    date: '2024-04-24',
+    startDate: '',
+    name: 'Wither Sword',
+    tagline: 'Weapon addition for the Butterfly SMP.',
+    images: [],
+    status: 'archived',
+    description: (
+      <>
+        <p>Added a custom Minecraft weapon with custom on-hit effects for the Butterfly SMP.</p>
+        <p>Built for Minecraft 1.20.1.</p>
+      </>
+    ),
+    technologies: ['Java', 'Spigot API'],
+    links: {},
     featured: false
   }
 ];
