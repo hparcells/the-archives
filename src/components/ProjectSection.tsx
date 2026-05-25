ea9717e5c49721a394af5d97a483ec8a19fae20d
Hunter Parcells <hunterparcells@gmail.com>
2026-05-25 11:29:29 -0400

    fix: Animate removing project section items.

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -43,6 +43,7 @@ function ProjectSection({ title, filter, filterValue, items, initialLimit }: Pro
             key={item.name}
             initial={{ height: 0, opacity: 0 }}
             animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
             transition={{ duration: 0.2, ease: 'easeOut' }}
           >
             <ProjectSectionItem item={item} />
